21918f952e7a69a3b7243712a121926a61d5a230
Harsh <harshnag23@gmail.com>
2022-09-09 00:52:06 +0530

    Template clone of sigup and login done.

--- clients/routes.py
+++ clients/routes.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template,flash,request
+from flask import redirect, render_template, flash, request
 from flask import Flask
 from clients import app
 
@@ -6,3 +6,13 @@ from clients import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
